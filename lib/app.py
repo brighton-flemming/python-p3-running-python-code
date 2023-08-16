ddfb8eee6b5183cbfbba13d31468f5b58dc1ed66
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-16 08:31:55 +0300

    Mission Passed

--- lib/app.py
+++ lib/app.py
@@ -4,3 +4,18 @@ print("Hallo Welt!", end="!! ")
 print("Olá Mundo!", end="!! ")
 print("你好世界！",  end="!!!\n")
 print("Hei Verden!",  end="!!!\n")
+
+print(f"'Hello World! ...st, please.'\n")
+print(f"Hello World! Pass this test, please.")
+
+
+
+
+
+
+
+
+
+     
+
+    
